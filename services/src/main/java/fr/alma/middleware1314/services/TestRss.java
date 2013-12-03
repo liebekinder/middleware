72b23d566571a46ce1b988bff802fab4effa4e27
Ornicare <antoine.floriano@gmail.com>
2013-12-03 10:52:10 +0100

    get non read articles

--- services/src/main/java/fr/alma/middleware1314/services/TestRss.java
+++ services/src/main/java/fr/alma/middleware1314/services/TestRss.java
@@ -14,8 +14,17 @@ import com.sun.syndication.io.XmlReader;
 public class TestRss {
 
 	public static void main(String[] args) throws Exception {
+		//proxy management
+        String proxyHost = "cache.etu.univ-nantes.fr";
+        String proxyPort = "3128";
+
+        System.setProperty("proxySet", "true");
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+		//end proxy management
+        
 //		URL source = new URL("http://linuxfr.org/news.atom");
-		URL source = new URL("http://boards.4chan.org/board/index.rss");
+		URL source = new URL("http://boards.4chan.org/b/index.rss");
 		SyndFeedInput input = new SyndFeedInput();
 		SyndFeed feed = input.build(new XmlReader(source));
 		// System.out.println(feed);
@@ -25,8 +34,8 @@ public class TestRss {
 		for (Iterator<?> i = feed.getEntries().iterator(); i.hasNext();) {
 			SyndEntry entry = (SyndEntry) i.next();
 			System.out.println(entry.getTitle());
-			System.out.println("---"+entry.getUri());
-			System.out.println(entry.getContents().get(0).getClass());
+			System.out.println("---"+entry.getContents());
+//			System.out.println(entry.getContents().get(0).getClass());
 		}
 
 	}
